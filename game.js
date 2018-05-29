7d1bee9fb840d17814cdb77f95e702212e1a1142
AMutaa <piusandru@gmail.com>
2018-05-29 15:20:59 -0400

    random picker

--- game.js
+++ game.js
@@ -8,24 +8,42 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-colorDisplay = document.getElementById("colorDisplay")
+var pickedColor = pickColor();
+colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
  // add initial colors to squares
  squares[i].style.backgroundColor = colors[i];
+
  // add click listeners to squares
  squares[i].addEventListener("click", function () {
   // grab color of clicked square
   var clickedColor = (this.style.backgroundColor);
-  if (clickedColor === pickedColor) {
-   alert("Correct!");
-  } else {
-   this.style.background = "#232323";
-  }
 
   // compare color of clicked square
+  if (clickedColor === pickedColor) {
+   messageDisplay.textContent = "Correct!"
+   changeColors(clickedColor);
+  } else {
+   this.style.background = "#232323";
+   messageDisplay.textContent = "TryAgain"
+
+  }
  });
+}
+
+function changeColors(color) {
+ // loop through all squares
+ for (var i = 0; i < squares.length; i++) {
+  // change each color to match given color
+  squares[i].style.backgroundColor = color;
+ }
+}
+
+function pickColor() {
+ var random = Math.floor(Math.random() * colors.length);
+ return colors[random];
 }
